31eacb7f81d369ba542ec33e90e978d93664b00a
Wendi Fan <wendi.fan@softwire.com>
2025-02-17 08:52:40 +0000

    Update initial setup: Fix package.json and draw initial grid

---
+++ frontend/scripts/game.js
@@ -0,0 +1,12 @@
+import createGame  from "./gameLogicInterface.js"
+import { drawGrid } from "./gameUI.js"
+
+initialiseGame();
+
+function initialiseGame() {
+    let game = createGame();
+
+    drawGrid();
+
+    console.log(game);
+}
