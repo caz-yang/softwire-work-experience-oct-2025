1a2146a4a12bc6270755afed101e534a3d00885b
Miles <milesjpwijeyertatne@gmail.com>
2025-02-19 15:02:59 +0000

    added column to database

--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -1,7 +1,7 @@
 import sql from '../../services/postgres.js';
 
 export const someAction = async (res, parameters) => {
-    await sql`INSERT INTO tetris_scores (score) VALUES (100)`;
+    await sql`INSERT INTO tetris_scores (player_name, score) VALUES ('foo', 100)`;
     const data = await sql`SELECT * FROM tetris_scores`;
     
     return  res.status(200).json({
