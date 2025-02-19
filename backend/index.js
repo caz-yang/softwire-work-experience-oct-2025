dfe4e997e21c1fdc7ed1f9e06cced1ac0e693e3b
Wendi Fan <wendi.fan@softwire.com>
2025-02-19 08:58:48 +0000

    Update db and backend code

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 routes(app);
 
 const port = process.env.PORT || 3000;
