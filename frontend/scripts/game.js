8188635e97192a6b7d73cc276f7d8bfd9e030041
IbrahimKalafat <ibrahimkalafat12@gmail.com>
2025-02-20 12:55:00 +0000

    rotations-updated

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -6,9 +6,19 @@ initialiseGame();
 function initialiseGame() {
     let game = createGame();
 
-    drawGrid();
-
-    
-  
-
-}
+    document.addEventListener("keyup", (event) => {
+        if (event.key === "ArrowUp"){
+            console.log("Up arrow has been pressed");{
+            game.rotateTetrominoClockwise();
+        }
+        if (event.key === "keyX"){
+            console.log("X key has been pressed");{
+            game.rotateTetrominoAnticlockwise();
+           
+        }
+          
+    drawGrid();    
+    }
+}  
+    });                 
+}     
