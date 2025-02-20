b1c5da1f4b8295ccd1f85f60613467ec49ad31f1
IbrahimKalafat <ibrahimkalafat12@gmail.com>
2025-02-20 15:30:40 +0000

    rotations keys fix

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -5,20 +5,16 @@ initialiseGame();
 
 function initialiseGame() {
     let game = createGame();
+    drawGrid();
 
     document.addEventListener("keyup", (event) => {
-        if (event.key === "ArrowUp"){
-            console.log("Up arrow has been pressed");{
+        console.log("Key pressed:", event.key);
+        if (event.key === "ArrowUp") {
+            console.log("Up arrow has been pressed");
             game.rotateTetrominoClockwise();
+        } else if (event.key === "x") {
+            console.log("X key has been pressed");
+            game.rotateTetrominoAntiClockwise();
         }
-        if (event.key === "keyX"){
-            console.log("X key has been pressed");{
-            game.rotateTetrominoAnticlockwise();
-           
-        }
-          
-    drawGrid();    
-    }
-}  
-    });                 
-}     
+    });
+}
