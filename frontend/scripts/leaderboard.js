7037ebf02885f915f2a43b2844e1faa407c36f84
lheiyu <leejody1011@gmail.com>
2025-02-21 11:31:04 +0000

    post request on window init of leaderboard page

--- frontend/scripts/leaderboard.js
+++ frontend/scripts/leaderboard.js
@@ -1,7 +1,18 @@
-import * from ".backend\src\controller\index.js"
-
-async function fetchAsync ("localhost:3000") {
-    let response = await fetch("localhost:3000");
-    let data = await response.json();
-    return data;
+async function fetchScores(host) {
+    let backendUrl = new URL(host);
+    backendUrl.port = 3000;
+    const res = await fetch(`${backendUrl.origin}/api/scores`);
+    const leaderboard = await res.json();
+    return leaderboard.data;
   }
+
+function leaderboard_scores () {
+    let scores = fetchScores("http://localhost");
+    console.log(scores);
+    let top_five_scores = scores[0, 4];
+    console.log(top_five_scores);
+}
+
+window.onload = function() {
+    leaderboard_scores();
+}
