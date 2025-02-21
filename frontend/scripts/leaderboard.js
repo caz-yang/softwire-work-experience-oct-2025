435963f89edba1cc763c9116a4526dd6ec4bbdce
Wendi Fan <wendi.fan@softwire.com>
2025-02-21 12:33:56 +0000

    Fix leaderboard page

--- frontend/scripts/leaderboard.js
+++ frontend/scripts/leaderboard.js
@@ -6,13 +6,21 @@ async function fetchScores(host) {
     return leaderboard.data;
   }
 
-function leaderboard_scores () {
-    let scores = fetchScores("http://localhost");
-    console.log(scores);
-    let top_five_scores = scores[0, 4];
-    console.log(top_five_scores);
+async function leaderboard_scores () {
+    let scores = await fetchScores("http://localhost");
+
+    let leaderboard_table = `"<table id="leaderboard"><tr><th>RANK</th><th>PLAYER</th><th>SCORE</th></tr>"`;
+
+    let index = 1;
+    scores.map(user => {
+        let tableRow = "<tr><td>" + index + "</td><td>" + user.player_name + "</td><td>" + user.score + "</td></tr>";
+        leaderboard_table += tableRow;
+        index++;
+    });
+    leaderboard_table += "</table>";
+  
+    let container = document.getElementById('leaderboard-container');
+    container.innerHTML = leaderboard_table;
 }
 
-window.onload = function() {
-    leaderboard_scores();
-}
+leaderboard_scores();
