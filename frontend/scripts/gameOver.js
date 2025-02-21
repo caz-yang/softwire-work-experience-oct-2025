0e47c2959588e9648f7b34bbc3370bc6392c66e6
Miles <milesjpwijeyertatne@gmail.com>
2025-02-21 11:24:40 +0000

    added save-score button

--- frontend/scripts/gameOver.js
+++ frontend/scripts/gameOver.js
@@ -6,3 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("score").textContent = score;
     }
 });
+
+document.getElementById("save-score-button").addEventListener("click", () => {
+    let name = document.getElementById("name-input").value;
+    let score = Number.parseInt(document.getElementById("score").textContent);
+
+    if (name == "") {
+        alert("cannot have an empty name");
+        return;
+    }
+
+    let data = {
+        "name": name,
+        "score": score
+    }
+
+    fetch("http://localhost:3000/api/save-score", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then( _ => {
+        let button = document.getElementById("save-score-button");
+        button.disabled = true;
+        button.textContent = "score saved"
+    });
+
+})
